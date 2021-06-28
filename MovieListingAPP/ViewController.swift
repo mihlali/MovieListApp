419e723579481d1fb8206765644fd7a6be7db87f
Mihlali Mazomba <mihlali.mazomba@absa.africa>
2021-06-28 19:47:05 +0200

    Initial Commit

---
+++ MovieListingAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListingAPP
+//
+//  Created by Mihlali Mazomba on 2021/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
